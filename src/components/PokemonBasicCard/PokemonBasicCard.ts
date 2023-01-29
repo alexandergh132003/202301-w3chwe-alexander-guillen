6c475ebb70995cd439ec8a3dacb5725f9c234fb6
Alexander <aguillenhernandez95@gmail.com>
2023-01-29 10:47:10 +0100

    Add pokemon card component

---
+++ src/components/PokemonBasicCard/PokemonBasicCard.ts
@@ -0,0 +1,33 @@
+import Component from "../Component/Component.js";
+import type { PokemonData } from "../../pokemons/types.js";
+
+export class PokemonBasicCardComponent extends Component {
+  private readonly pokemon: PokemonData;
+
+  constructor(parentElement: HTMLElement, pokemon: PokemonData) {
+    super(parentElement, "pokemon-card", "div");
+    this.pokemon = pokemon;
+  }
+
+  render() {
+    super.render();
+
+    this.domElement.innerHTML = `
+      <img src="${this.pokemon.sprites.other["official-artwork"].front_default}" alt="${this.pokemon.name}" class="pokemon-image" width="300" height="300">
+  
+      <div class="pokemon-card-basic-info">
+        <span class="pokemon-number">N.º ${this.pokemon.id}</span>
+        <h2 class="pokemon-name">${this.pokemon.name}</h2>
+        <span class="pokemon-types">
+        </span>  
+      </div>
+    `;
+
+    this.pokemon.types.forEach(({ type: { name } }) => {
+      const parentElement = this.domElement.querySelector(".pokemon-types");
+      const pokemonType = document.createElement("span");
+      pokemonType.textContent = name;
+      parentElement.appendChild(pokemonType);
+    });
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import PageComponent from "./components/PageComponent/PageComponent";
+
+const container = new PageComponent(document.body);
+
+container.render();
